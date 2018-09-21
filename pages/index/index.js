0df55aec164636e690d6841acb24f04d65d90248
Cathe97 <735602733@qq.com>
2018-09-21 18:59:16 +0800

    change type

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,8 +19,22 @@ Page({
     this.getTopnews(sortMap[0])
   },
 
+//设置新闻类名列表
+  setTypeList() {
+    let typeList = []
+    for (let i = 0; i < 7; i++) {
+      typeList.push({
+        key: sortMap[i],
+        value: sortChMap[i],
+      })
+    }
+    this.setData({
+      typeList: typeList
+    })
+  },
+
+//请求并设置头条新闻的数据
   getTopnews(sth) {
-    //请求头条新闻的数据
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
@@ -41,20 +55,9 @@ Page({
     })
   },
 
-  setTypeList(){
-    let typeList=[]
-    for(let i=0;i<7;i++){
-      typeList.push({
-        key:sortMap[i],
-        value:sortChMap[i],
-      })
-    }
-    this.setData({
-      typeList:typeList
-    })
-  },
-
+//设置新闻列表数据
   setNewsList(result){
+    //渲染新闻列表
     let newsList=[]
     for(let i=1;i<result.length;i++){
       newsList.push({
@@ -67,9 +70,14 @@ Page({
     this.setData({
       newsList:newsList
     })
+  },
+
+//点击其他类目的时候重新请求相应的头条以及新闻列表数据
+  onTapType(event){
+    let newsType=event.currentTarget.id
+    this.getTopnews(newsType)
   }
 
-
-
+//点击具体新闻进入详情页
 
 })
